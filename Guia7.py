dcd5cf1675111f8232e8d911565f78d99aa85628
tomaslibson <165915298+tomaslibson@users.noreply.github.com>
2024-05-27 11:20:03 -0300

    Update Guia7.py

--- Guia7.py
+++ Guia7.py
@@ -90,27 +90,24 @@ def capicua (palabra: str) -> bool:
 #1.7
 
 def tiene_minuscula (c: str) -> bool:
+    res = False
     for char in c:
-        if("a" <= char <="z"):
-           res=True
-        else: 
-            res=False
+        if("a" <= char and char <="z"):
+           res = True
     return res
       
 def tiene_mayus(c: str) -> bool:
+    res = False
     for char in c:
-        if("A" <= char <="Z"):
+        if("A" <= char and char <="Z"):
            res=True
-        else:
-            res=False
     return res
 
 def tiene_numero(c: str) -> bool:
+    res = False
     for num in c:
-        if ("0" <= num <="9"):
+        if ("0" <= num) and (num <="9"):
             res=True
-        else:
-            res=False
     return res
 
 def es_Verde(c: str) -> bool:
@@ -133,6 +130,161 @@ def fortaleza_contraseña (c: str) -> str:
 #print(fortaleza_contraseña ("12Ar")) #roja
 #print(fortaleza_contraseña ("123rrrrrrrr")) #amarilla
 #print(fortaleza_contraseña ("123Ar2565y")) #verde    
+
+#1.8
+
+def saldo_actual(list)-> int:
+    res = 0
+    for tuplas in list:
+        if (tuplas[0] == "I"):
+          res+= tuplas [1]
+        elif (tuplas[0] == "R"):
+            res-= tuplas [1]
+    return res
+
+#print (saldo_actual ([("I", 2000), ("R", 20),("R", 1000),("I", 300)]))
+
+     
+#1.9
+
+def tiene_3_vocales(string: str)->bool:
+    vocales = ["a","e","i","o","u"]
+    c = 0
+    for l in string:
+        if l.lower() in vocales:
+            c+=1
+            vocales.remove(l.lower())  
+        
+    return c >= 3
+          
+
+    
+#print (tiene_3_vocales("aeio"))
+
+
+##EJERCICIO 2
+
+def coloca_0_en_par(s:list):
+    i = 0
+    while i < len(s):
+        if i % 2 == 0:
+            s[i] = 0
+        i+=1    
+    return s                   
+
+#print (coloca_0_en_par([1,2,3,4,1,1,1,1,1,1,1]))
+
+#2.2
+
+def coloca_0_en_par_lista(s:list):
+    i = 0
+    l = []
+    while i < len(s):
+        if i % 2 == 0:
+            l.append(0)
+        else:
+            l.append(s[i])     
+        i+=1   
+    return l
+
+#print (coloca_0_en_par_lista([1,2,3,4,1,1,1,1,1,1,1]))
+
+#2.3 
+
+def cadena_sin_vocales(s:str):
+    vocales = ["a","e","i","o","u"]
+    l = ""
+    for c in s:
+        if c not in vocales:
+            l+=c
+    return l
+
+#print (cadena_sin_vocales ("abce"))
+
+#2.4
+
+def remplaza_vocales(s:str):
+    vocales = ["a","e","i","o","u"]
+    l = ""
+    for c in s:
+        if c in vocales:
+           l+="-"
+        else:
+            l+=c
+    return l
+
+#print (remplaza_vocales ("hola"))
+
+#2.5
+
+def da_vuelta_str(s:str):
+    i = len(s) - 1
+    l = ""
+    while i >= 0:
+      l+= s[i]
+      i-=1
+    return l
+
+#print (da_vuelta_str ("no es la nave es el piloto"))  
+
+#2.6
+
+def eliminar_repetidos(s:str):
+    l = ""
+    for c in s:
+        if c not in l:
+            l+=c
+    return l
+
+#print (eliminar_repetidos ("hhhhooohhhlllaaa"))
+
+#EJERCICIO 3
+
+def aprobado(list: int)->int:
+    k= 0
+    i = 0
+    res = 0
+ 
+    while i < len(list):
+        k+=list[i]
+        i+=1 
+
+    promedio = k / len(list) 
+    
+    for notas in list:
+        if notas < 4:
+            return 3
+
+    if  promedio < 4:
+            res = 3
+    elif promedio >= 4 and promedio < 7:
+            res= 2
+    elif promedio >= 7:
+            res = 1
+    return res 
+
+#print (aprobado ([7,6]))
+
+#EJERCICIO 4
+
+def nombres_estudiantes() -> list:
+    res = []
+    listo = False
+     
+    while not listo:
+      nombre = input("Ingrese un nombre: ")
+      if nombre == "listo":
+          listo = True
+      else:
+          res.append(nombre)    
+    return res
+
+#print(nombres_estudiantes())
+
+#4.2
+
+
+
 
 
 print (tiene_minuscula("123Ar2565y"))
